1288d7d0d11b86609dc72bcbb70decc556dffbce
sathasis <1satheeshkumars@gmail.com>
2021-03-06 19:45:04 +0530

    second commit

--- src/training/Product.java
+++ src/training/Product.java
@@ -15,7 +15,7 @@ public class Product {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int sum = 4 + 4 ;
-		int a = 9;
+		
 		System.out.println(sum);
 	}
 
--- src/training/seccond.java
+++ src/training/seccond.java
@@ -4,7 +4,8 @@ public class seccond {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int count = 9 ;
+		int sum = count + 1;
 	}
 
 }
