ec5a2d110b6dbce6b8733ef6ac77a777e99e45ca
sathasis <1satheeshkumars@gmail.com>
2021-03-06 12:35:34 +0530

    First commit

---
+++ src/training/Product.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package training;
+
+/**
+ * @author sathsis
+ *
+ */
+public class Product {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 4 + 4 ;
+		System.out.println(sum);
+	}
+
+}
